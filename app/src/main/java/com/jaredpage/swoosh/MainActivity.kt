468a0f005cc4b136c9d52d84719d3786c59c44ec
JaredGPage <jared.page10@gmail.com>
2022-06-03 19:56:14 +0200

    Created intent to league page

--- app/src/main/java/com/jaredpage/swoosh/MainActivity.kt
+++ app/src/main/java/com/jaredpage/swoosh/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.jaredpage.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        getStartedBtn.setOnClickListener{
+            val leagueIntent = Intent(this, LeagueActivity::class.java) //intent allows you to interact with other applications and your own, this is explicit - in own app
+            startActivity(leagueIntent)
+        }
     }
 }
